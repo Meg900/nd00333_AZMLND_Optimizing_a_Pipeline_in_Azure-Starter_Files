f40563a380a8a99eb027a5d1a1c97b2c0b662798
Meg900 <73169608+Meg900@users.noreply.github.com>
2020-10-20 16:41:15 +0600

    Add files via upload

--- train.py
+++ train.py
@@ -14,13 +14,8 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
+web_path = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+ds = TabularDatasetFactory.from_delimited_files(path = web_path)
 
 run = Run.get_context()
 
@@ -50,23 +45,30 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+    return x_df, y_df
 
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument('--C', type = float, default = 1.0, help = "Inverse of regularization strength. Smaller values cause stronger regularization")
+    parser.add_argument('--max_iter', type = int, default = 100, help = "Maximum number of iterations to converge")
 
     args = parser.parse_args()
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
-
+    
+    x, y = clean_data(ds)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33, random_state = 42)
+    
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
-
+    
+    os.makedirs('outputs', exist_ok = True)
+    joblib.dump(model, 'outputs/model.joblib')
+    
 if __name__ == '__main__':
     main()
